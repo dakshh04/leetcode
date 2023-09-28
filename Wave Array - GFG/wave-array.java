785bd601576b20cdf9b0fb09cdb9df8ade830708
Dakshh Singh <dakshhsingh4902@gmail.com>
2023-09-28 10:00:59 +0530

    Added solution - LeetHub

--- Wave Array - GFG/wave-array.java
+++ Wave Array - GFG/wave-array.java
@@ -60,11 +60,11 @@ class GFG {
 class Solution {
     public static void convertToWave(int n, int[] arr) {
         // code here
-        int temp;
-        for(int i = 0; i < arr.length - 1; i = i + 2){
-            temp = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = temp;
+        
+        for(int i = 0; i < n-1; i= i + 2){
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
         }
     }
 }
